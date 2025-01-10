e887adb4304fdaefb0c3e8e324eb32658995b918
Hassaaan1010 <hassaaan1010@gmail.com>
2025-01-11 05:12:33 +0530

    useEffect bug fixed

--- client/src/components/login.jsx
+++ client/src/components/login.jsx
@@ -1,7 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useNavigate, Link } from "react-router-dom";
-import { useEffect } from "react";
 
 const Login = () => {
   const baseUrl = import.meta.env.VITE_APP_BASE_URL || "http://localhost:4000";
